ad078e13d1715b7b3fa9e95964cbd0c0e90025c2
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-08-10 15:15:45 +0530

    admin sign in In function done

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+
+
+function adminSignIn(){
+
+    var adminEmail = document.getElementById("adminEmail");
+    var adminPassword = document.getElementById("adminPassword");
+
+
+    var f = new FormData();
+    f.append("e", adminEmail.value);
+    f.append("p", adminPassword.value);
+    
+
+    var r = new XMLHttpRequest();
+
+    r.onreadystatechange = function () {
+        if (r.readyState == 4) {
+            var t = r.responseText;
+            if (t == "success") {
+                window.location = "adminHome.php";
+                alert(t);
+            } else {
+                document.getElementById("msg").innerHTML = t;
+                alert(t);
+            }
+
+        }
+    };
+
+    r.open("POST", "adminSignInProcess.php", true);
+    r.send(f);
+
+}
