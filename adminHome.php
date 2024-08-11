6f456507e01072f1d6c1ba006bd829859ab59d7c
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-08-11 20:18:08 +0530

    change

--- adminHome.php
+++ adminHome.php
@@ -1,48 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Home</title>
 </head>
+
 <body>
-    
-<div class=" container-fluid">
-<div class=" row">
 
-<?php include "header.php"; ?>
-<hr class=" my-0">
+    <div class=" container-fluid">
+        <div class=" row">
 
-<div class="col-2 col-lg-2 pt-5 px-3 text-light" style="background-color: #12492f; height: 100vh;">
+            <?php include "header.php"; ?>
+            <hr class=" my-0">
 
-<p class="col-12 ">Latest News Update</p>
+            <div class="col-2 col-lg-2 pt-5 px-3 text-light mx-0" style="background-color: #12492f; height: 100vh;">
 
-</div>
+                <button class=" col-12 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView1();">Appointment Details</button>
+                <button class=" col-12 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView2();">Latest News Update</button>
 
-<div class=" col-8 col-lg-10" style="background-color: #c2ffff;" id="newsUpdateBox">
-    
-<div class="row">
+            </div>
 
-<div class="col-4">
+            <!-- Appointment Details page import to admin home page-->
+
+            <div class="col-10 col-lg-10 " id="appointmentBox">
+                <?php include "appointmentDetails.php"; ?>
+
+
+            </div>
+
+            <!--Latest News update form -->
+
+            <div class="col-10 col-lg-10 d-none " style="background-color: #eee; height:20vh" id="newsBox">
+
+
+            </div>
 
 
 
-</div>
-<div class="col-4">
 
-</div>
-<div class="col-4">
 
-</div>
 
-</div>
+            <?php include "footer.php"; ?>
 
-</div>
+        </div>
+    </div>
 
-<?php include "footer.php"; ?>
 
-</div>
-</div>
-
+    <script src="script.js"></script>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -47,3 +47,45 @@ function adminSignIn(){
     r.send(f);
 
 }
+
+
+
+function changeView1(){
+
+   
+    var appointmentBox= document.getElementById("appointmentBox");
+
+    
+if(!appointmentBox.classList.toggle("d-none")){
+
+    appointmentBox.classList.toggle("d-block");
+    newsBox.classList.toggle("d-none");
+}
+else{
+    appointmentBox.classList.toggle("d-none");
+    
+}
+ 
+  
+
+}
+
+function changeView2(){
+
+    var newsBox = document.getElementById("newsBox");
+
+    if(!newsBox.classList.toggle("d-none")){
+
+        appointmentBox.classList.toggle("d-none");
+        newsBox.classList.toggle("d-block");
+        
+    }
+    else{
+        newsBox.classList.toggle("d-none")
+
+    }
+    
+     
+
+}
+
