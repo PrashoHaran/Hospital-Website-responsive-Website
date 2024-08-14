4b0032f11e7f229f3d4681ce76ce7b05b69a2b7e
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-08-14 12:32:26 +0530

    create latestNews.php file

---
+++ latestNews.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latest News Update</title>
+</head>
+
+<body>
+
+    <div class=" container-fluid">
+        <div class=" row">
+
+            <!--Latest News Card 1 -->
+            <div class=" col-12 col-lg-4 ">
+
+                <div class="col-12 ">
+                    <label for="" class=" form-check-label my-3">Latest News card 1 Image</label>
+                </div>
+                <div class="col-12">
+
+                    <div class="input-group">
+                        <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                        <!-- <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Button</button>-->
+                    </div>
+                </div>
+                <!--Card Title -->
+                <div class="col-12"><label for="">Card 1 Title</label></div>
+                <div class="col-12"><input type="text" name="" id="" class="form-control"></div>
+
+                <!--Description -->
+                <div class="col-12"><label for="">Add small description</label></div>
+                <div class="col-12"><textarea class="form-control" aria-label="With textarea"></textarea></div>
+
+            </div>
+
+            <!--Latest News Card 2 -->
+
+            <div class=" col-12 col-lg-4 ">
+
+                <div class="col-12 ">
+                    <label for="" class=" form-check-label my-3">Latest News card 2 Image</label>
+                </div>
+                <div class="col-12">
+
+                    <div class="input-group">
+                        <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                        <!-- <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Button</button>-->
+                    </div>
+                </div>
+                <!--Card Title -->
+                <div class="col-12"><label for="">Card 2 Title</label></div>
+                <div class="col-12"><input type="text" name="" id="" class="form-control"></div>
+
+                <!--Description -->
+                <div class="col-12"><label for="">Add small description</label></div>
+                <div class="col-12"><textarea class="form-control" aria-label="With textarea"></textarea>
+
+
+                </div>
+
+                <!--Latest News Card 3 -->
+
+
+            </div>
+
+
+            <div class=" col-12 col-lg-4">
+
+                <div class="col-12">
+                    <label for="" class=" form-check-label my-3">Latest News card 3 Image</label>
+                </div>
+                <div class="col-12">
+
+                    <div class="input-group">
+                        <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                        <!-- <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Button</button>-->
+                    </div>
+                </div>
+                <!--Card Title -->
+                <div class="col-12"><label for="">Card 3 Title</label></div>
+                <div class="col-12"><input type="text" name="" id="" class="form-control"></div>
+
+                <!--Description -->
+                <div class="col-12"><label for="">Add small description</label></div>
+                <div class="col-12"><textarea class="form-control" aria-label="With textarea"></textarea></div>
+
+
+            </div>
+        </div>
+
+
+
+</body>
+
+</html>
