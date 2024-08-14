b0d6d30cc6c588ba4925cd25b1787e6b499ef914
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-08-14 12:32:37 +0530

    make changes

--- adminHome.php
+++ adminHome.php
@@ -15,16 +15,16 @@
             <?php include "header.php"; ?>
             <hr class=" my-0">
 
-            <div class="col-2 col-lg-2 pt-5 px-3 text-light mx-0" style="background-color: #12492f; height: 100vh;">
+            <div class="col-12 col-lg-12 pt-1 px-3 text-light mx-0" style="background-color: #12492f;">
 
-                <button class=" col-12 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView1();">Appointment Details</button>
-                <button class=" col-12 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView2();">Latest News Update</button>
+                <button class=" col-4 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView1();">Appointment Details</button>
+                <button class=" col-4 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView2();">Latest News Update</button>
 
             </div>
 
             <!-- Appointment Details page import to admin home page-->
 
-            <div class="col-10 col-lg-10 " id="appointmentBox">
+            <div class="col-12 col-lg-12" id="appointmentBox">
                 <?php include "appointmentDetails.php"; ?>
 
 
@@ -32,9 +32,11 @@
 
             <!--Latest News update form -->
 
-            <div class="col-10 col-lg-10 d-none " style="background-color: #eee; height:20vh" id="newsBox">
+            <div class="col-12 col-lg-12 d-none " style="background-color: #eee;" id="newsBox">
 
-<!-- menna me div eka athule latest news update tika hdnna -->
+                <!-- menna me div eka athule latest news update tika hdnna -->
+
+                <?php include "latestNews.php" ?>
 
             </div>
 
