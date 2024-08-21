ad59650e4c3f80b9c6d004abbebd9112998b5235
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-08-21 23:05:02 +0530

    create AppointmentSearch file

---
+++ AppointmentSearch.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointment Search</title>
+</head>
+<body>
+    
+<div class=" container-fluid">
+<div class=" row">
+
+<?php include "header.php"; ?>
+<hr class=" my-0">
+
+
+<div class="col-6 col-lg-4 mt-4 mb-4 mx-lg-5">
+
+<select name="" id="" class=" form-select">
+
+<option value="" disabled selected>Select Doctor</option>
+<option value=""> Janaka</option>
+
+</select>
+
+</div>
+
+<div class="col-6 col-lg-4 mt-4 mb-4">
+
+<select name="" id="" class=" form-select">
+
+<option value="" disabled selected>Select a speciality</option>
+<option value="">Ayurvedic</option>
+<option value="">Cardiologist</option>
+
+</select>
+
+</div>
+
+
+<div class=" col-12 col-lg-12">
+
+
+
+</div>
+
+
+
+
+</div>
+
+</div>
+
+</body>
+</html>
