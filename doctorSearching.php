8b9545a3883b4d69d1d62d2770373c994f4af53b
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-08-25 17:57:29 +0530

    change

---
+++ doctorSearching.php
@@ -0,0 +1,28 @@
+<?php
+require "connection.php";
+
+if (isset($_POST['special_id'])) {
+    $special_id = $_POST['special_id'];
+
+    // Fetch doctors based on the selected specialization id
+   
+    
+$result = Database::search("SELECT d.dName FROM doctor d INNER JOIN specialization s ON d.special_id = s.id WHERE s.id = '".$special_id."'");
+
+if($result->num_rows > 0){
+
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        echo '<option value="'.$row['dName'].'">Dr.'.$row['dName'].'</option>';
+        
+        }
+}
+else{
+    echo '<option value="" disabled>No Doctors Available</option>';
+
+}
+
+ 
+ 
+}
+?>
