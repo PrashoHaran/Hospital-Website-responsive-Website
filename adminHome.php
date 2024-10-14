cb9141093147c1a919b2ac716cc85d188574986b
Gihan sanjula <sanjulagihan94@gmail.com>
2024-10-14 20:42:19 +0530

    #

--- adminHome.php
+++ adminHome.php
@@ -20,7 +20,7 @@
                 <button class=" col-3 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView1();">Appointment Details</button>
                 <button class=" col-3 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView2();">Latest News Update</button>
                 <button class=" col-3 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="window.location.href='addDoctor.php';">Add Doctor</button>
-
+                
             </div>
 
             <!-- Appointment Details page import to admin home page-->
