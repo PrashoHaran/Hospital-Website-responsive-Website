52f8144c3eba5c319e93e4c9ed537cdafde275cc
OshanKothalawala <125176122+AkilaShashimantha@users.noreply.github.com>
2024-10-14 19:35:44 +0530

    #

--- adminHome.php
+++ adminHome.php
@@ -15,12 +15,12 @@
             <?php include "header.php"; ?>
             <hr class=" my-0">
 
-            <div class="col-12 col-lg-12 pt-1 px-0 text-light mx-0" style="background-color: #12492f;">
+            <div class="col-12 col-lg-12 pt-1 px-0 text-light mx-0 p-0" style="background-color: #12492f;">
 
                 <button class=" col-3 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView1();">Appointment Details</button>
                 <button class=" col-3 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="changeView2();">Latest News Update</button>
                 <button class=" col-3 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="window.location.href='addDoctor.php';">Add Doctor</button>
-
+                <button class=" col-2 btn btn-light text-light btn-btn-outline-light my-2" style="background:  linear-gradient(90deg, rgba(25,158,9,1) 0%, rgba(13,37,14,1) 50%, rgba(22,170,13,1) 100%);" onclick="window.location.href='addLabReport.php';">Add Lab Report</button>
             </div>
 
             <!-- Appointment Details page import to admin home page-->
@@ -28,14 +28,14 @@
             <div class="col-12 col-lg-12" id="appointmentBox">
                 <?php include "appointmentDetails.php"; ?>
 
-                <?php include "footer.php"; ?>
+               
             </div>
 
             <!--Latest News update form -->
 
             <div class="col-12 col-lg-12 d-none " style="background-color: #eee;" id="newsBox">
 
-                <!-- menna me div eka athule latest news update tika hdnna -->
+         
 
                 <?php include "updateForm.php" ?>
 
@@ -45,7 +45,7 @@
 
 
 
-
+            <?php include "footer.php"; ?>
        
 
         </div>
--- index.php
+++ index.php
@@ -38,7 +38,8 @@ $resultset = Database::search("SELECT card_id, title, text_content, image_path, 
       <hr class="my-0">
 
 
-      <nav class="navbar navbar-dark col-sm-12 mt-0" style="background-color: #26905E;">
+
+      <nav class="navbar navbar-dark col-sm-12 mt-0" style="background-image: linear-gradient(to right top, #120946, #1d1b64, #262d85, #2b41a7, #2b56cb);">
         <div class="container-fluid">
 
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
@@ -116,116 +117,121 @@ $resultset = Database::search("SELECT card_id, title, text_content, image_path, 
         </div>
       </nav>
 
+      <div style="background-image: linear-gradient(to right top, #f3f3f3, #f4f4f5, #f4f6f8, #f4f7fa, #f4f9fc);">
 
-      <div class="col-12 offset-lg-1 col-lg-10 mt-2">
+        <div class="col-12 offset-lg-1 col-lg-10 mt-2">
 
-        <div class="row">
-          <!--  carousel images  Auto scrolling -->
+          <div class="row">
+            <!--  carousel images  Auto scrolling -->
 
-          <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
-            <div class="carousel-inner ">
+            <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
+              <div class="carousel-inner ">
 
-              <div class="carousel-item active">
-                
-                <img src="images/img1.jpg" class="d-block w-100" alt="..."> 
+                <div class="carousel-item active">
 
-              <!--  <div class="image-1-display">
+                  <img src="images/img1.jpg" class="d-block w-100" alt="...">
+
+                  <!--  <div class="image-1-display">
                 <div class="image-1-text">
                   <div class="image-1-title">Where Healing Begins with Heart, Your Trusted Partner in Health</div>
                 </div>
                 </div>-->
 
-              </div>
+                </div>
 
-              <div class="carousel-item">
-                 <img src="images/img2.jpg" class="d-block w-100" alt="..."> 
+                <div class="carousel-item">
+                  <img src="images/img2.jpg" class="d-block w-100" alt="...">
 
-              <!--  <div class="image-2-display">
+                  <!--  <div class="image-2-display">
                 <div class="image-2-text">
                   <div class="image-2-title">Caring for the Community, Committed to You</div>
                 </div>
                 </div> -->
 
-              </div>
+                </div>
 
-              <div class="carousel-item">
-                <img src="images/img3.jpg" class="d-block w-100" alt="..."> 
+                <div class="carousel-item">
+                  <img src="images/img3.jpg" class="d-block w-100" alt="...">
 
-               <!-- <div class="image-3-display">
+                  <!-- <div class="image-3-display">
                 <div class="image-3-text">
                   <div class="image-3-title">Excellence in Healthcare, Every Day. Together for a Healthier Tomorrow!</div>
                 </div>
                 </div> -->
 
-              </div>
-
-            </div>
-          </div>
-
-         
-
-           <div class="col-12 offset-lg-2 col-lg-8 mt-3 my-3">
-
-          <div class="row">
-
-
-            <div class="card p-3 w-100" >
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="images/img5.jpg" class="img-fluid rounded-start" alt="...">
                 </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">International Patient Care </h5>
-                    <p class="card-text">Prime Care Hospital has a dedicated team to handle the care of international patients within the hospital. This includes coordinating not only the patient’s stay at the hospital and insurance coverage but also airport drop off and pickups as well as hotel accommodation. </p>
-                    <p class="card-text"><small class="text-body-secondary"></small></p>
+
+              </div>
+            </div>
+
+
+
+            <div class="col-12 offset-lg-2 col-lg-8 mt-3 my-3">
+
+              <div class="row">
+
+
+                <div class="card p-3 w-100">
+                  <div class="row g-0">
+                    <div class="col-md-4">
+                      <img src="images/img5.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">International Patient Care </h5>
+                        <p class="card-text">Prime Care Hospital has a dedicated team to handle the care of international patients within the hospital. This includes coordinating not only the patient’s stay at the hospital and insurance coverage but also airport drop off and pickups as well as hotel accommodation. </p>
+                        <p class="card-text"><small class="text-body-secondary"></small></p>
+                      </div>
+                    </div>
                   </div>
                 </div>
+
               </div>
+
             </div>
 
-          </div>
-
-        </div>
-    
 
 
-        <div class="col-12">
-            <div class="card-group">
+            <div class="col-12">
+              <div class="card-group">
                 <?php
                 if ($resultset->num_rows > 0) {
-                    while ($row = $resultset->fetch_assoc()) {
-                        ?>
-                        <div class="card" id="<?php echo $row['card_id']; ?>">
-                            <img src="<?php echo $row['image_path']; ?>" class="card-img-top" alt="Card image">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $row['title']; ?></h5>
-                                <p class="card-text"><?php echo $row['text_content']; ?></p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated <?php echo $row['last_updated']; ?></small></p>
-                            </div>
-                        </div>
-                        <?php
-                    }
+                  while ($row = $resultset->fetch_assoc()) {
+                ?>
+                    <div class="card" id="<?php echo $row['card_id']; ?>">
+                      <img src="<?php echo $row['image_path']; ?>" class="card-img-top" alt="Card image">
+                      <div class="card-body">
+                        <h5 class="card-title"><?php echo $row['title']; ?></h5>
+                        <p class="card-text"><?php echo $row['text_content']; ?></p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated <?php echo $row['last_updated']; ?></small></p>
+                      </div>
+                    </div>
+                <?php
+                  }
                 } else {
-                    echo "<p>No cards available.</p>";
+                  echo "<p>No cards available.</p>";
                 }
                 ?>
+              </div>
             </div>
+          </div>
+
+
+   
+
+
         </div>
+
       </div>
-
-
-      <hr>
-
-      <?php include "footer.php"; ?>
-      
-     
-      
+      <hr class=" my-0">
     </div>
+</div>
 
-  </div>
+    <?php include "footer.php"; ?>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
